0c727de3132a059405dc38821dcdc0a953c80767
Kumar Vaibhav <kumarvaibhav.official.1307@gmail.com>
2022-07-14 08:04:34 +0530

    program submission for 14th July

---
+++ Submissions/14th July, 2022/program1.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    // https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+    // 105. Construct Binary Tree from Preorder and Inorder Traversal
+    private int index = 0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildTree(preorder, inorder, 0, preorder.length - 1);
+    }
+
+    private TreeNode buildTree(int[] preorder, int[] inorder, int start, int end) {
+        // if there are no left and right children
+        if (end < start)
+            return null;
+
+        TreeNode tree = new TreeNode(preorder[index++]);
+        // if(start == end) return tree;
+
+        int rootIndex = findIndex(inorder, start, end, tree.val);
+
+        tree.left = buildTree(preorder, inorder, start, rootIndex - 1);
+        tree.right = buildTree(preorder, inorder, rootIndex + 1, end);
+        return tree;
+    }
+
+    private int findIndex(int[] inorder, int start, int end, int val) {
+        while (start < end && inorder[start] != val && inorder[end] != val) {
+            start++;
+            end--;
+        }
+        return inorder[start] == val ? start : end;
+    }
+}
